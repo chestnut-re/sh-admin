4099ce4e0849f44fc01e8e57db3a02dc1e9c3a89
邓晓坤 <dengxiaokun@openseas.com.cn>
2021-12-20 11:28:01 +0800

    build：dayjs replace moment

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const copyWebpackPlugin = require('copy-webpack-plugin')
-
+const AntdDayjsWebpackPlugin =require('antd-dayjs-webpack-plugin');
 const imageInlineSizeLimit = parseInt(process.env.IMAGE_INLINE_SIZE_LIMIT || '10000')
 
 module.exports = function (webpackEnv) {
@@ -162,6 +162,7 @@ module.exports = function (webpackEnv) {
           },
         ],
       }),
+      new AntdDayjsWebpackPlugin()
     ],
   }
 }
