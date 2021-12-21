3390a35f145dafbf11f999c23103900241a61222
fangmingdong <fangmingdong0224@gmail.com>
2021-12-21 10:46:15 +0800

    fix: Auto commit by fe-cli.

---
+++ src/service/TemplateService.ts
@@ -0,0 +1,47 @@
+import axios from '@/request'
+
+/**
+ * 模版 数据
+ */
+export class TemplateService {
+  /**
+   * 列表
+   */
+  static list({ size = 30, current }): Promise<any> {
+    return axios.get('/api/operation/banner/page', {
+      params: {
+        size,
+        current,
+      },
+    })
+  }
+
+  /**
+   * 删除
+   */
+  static del({ id }): Promise<any> {
+    return axios.delete(`/api/operation/banner/delete/${id}`)
+  }
+
+  /**
+   * 新增
+   */
+  static add(data): Promise<any> {
+    return axios.post(`/api/operation/banner/save`, {
+      params: {
+        data,
+      },
+    })
+  }
+
+  /**
+   * 编辑
+   */
+  static edit(data): Promise<any> {
+    return axios.put(`/api/operation/banner/update`, {
+      params: {
+        data,
+      },
+    })
+  }
+}
