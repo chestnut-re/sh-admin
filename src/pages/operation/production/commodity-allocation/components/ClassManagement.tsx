0bf4f4b520199c95ca947bea0870171364307d57
huanghaotong <1813698537@openseas.com.cn>
2021-12-24 15:41:13 +0800

    feat:商品配置

--- src/pages/operation/production/commodity-allocation/components/ClassManagement.tsx
+++ src/pages/operation/production/commodity-allocation/components/ClassManagement.tsx
@@ -1,33 +1,40 @@
-import React, { useState, useEffect } from 'react'
-import { Button, Tree } from 'antd'
+import React, { useState, useEffect, useRef } from 'react'
+import { Button, Tree, Modal, Input, Row, Col, Select } from 'antd'
 import './index.less'
+import AddDialog from './AEDialog'
+import { AllocationService } from '@/service/AllocationService'
+import TreePage from './tree'
+import { HttpCode } from '@/constants/HttpCode'
 /**
  * 分类管理
  */
 const ClassManagement: React.FC = () => {
-  const onSelect = (selectedKeys, info) => {
-    console.log('selected', selectedKeys, info)
+  const _ref = useRef()
+  const { Option } = Select
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [addClassName, setAddClassName] = useState('')
+  const [parentList, setParentList] = useState([])
+  const [parent, setParent] = useState('')
+
+  const handleOk = () => {
+    AllocationService.edit([{ operationType: 1, sortName: addClassName }]).then((res) => {
+      if (res.code === HttpCode.success) {
+        setIsModalVisible(false)
+        _ref.current?.getTreeList()
+      }
+    })
   }
-  const treeData = [
-    {
-      title: '跟团游',
-      key: '0-0',
-      children: [
-        { title: '亲自', key: '0-0-0', isLeaf: true },
-        { title: '三亚', key: '0-0-1', isLeaf: true },
-        { title: '夕阳', key: '0-0-2', isLeaf: true },
-      ],
-    },
-    {
-      title: '自由行',
-      key: '0-1',
-      children: [{ title: '欧美', key: '0-1-0', isLeaf: true }],
-    },
-  ]
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+  const _addClass = () => {
+    setIsModalVisible(true)
+  }
   return (
     <div className="class__root">
       <div className="class-btn">
-        <Button>添加商品分类</Button>
+        <Button onClick={_addClass}>添加商品分类</Button>
       </div>
       <div className="class-list">
         <div className="list-title">
@@ -35,13 +42,54 @@ const ClassManagement: React.FC = () => {
           <span>操作</span>
         </div>
         <div className="list-tree">
-          <Tree onSelect={onSelect} treeData={treeData} />
+          <TreePage cRef={_ref} />
         </div>
       </div>
       <div className="class-bottom">
         <Button type="primary">保存</Button>
         <Button>取消</Button>
       </div>
+      <Modal
+        title="产品分类名称"
+        visible={isModalVisible}
+        okText="确定"
+        cancelText="取消"
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <div>
+          <Row gutter={[0, 8]} style={{ marginBottom: 10 }}>
+            <Col span={8} className="table-from-label">
+              <span>分类名称&nbsp;&nbsp;</span>
+            </Col>
+            <Col span={16}>
+              <Input
+                style={{ width: 200 }}
+                value={addClassName}
+                onChange={(e) => {
+                  setAddClassName(e.target.value)
+                }}
+              />
+            </Col>
+          </Row>
+          <Row gutter={[0, 8]}>
+            <Col span={8} className="table-from-label">
+              <span>选择上级分类&nbsp;&nbsp;</span>
+            </Col>
+            <Col span={16}>
+              <Select placeholder="请选择" style={{ width: 120 }} onChange={(value: any) => setParent(value)}>
+                {parentList.map((item) => {
+                  return (
+                    <Option value={item.sortName} key={item.id}>
+                      {item.sortName}
+                    </Option>
+                  )
+                })}
+              </Select>
+            </Col>
+          </Row>
+        </div>
+      </Modal>
     </div>
   )
 }
