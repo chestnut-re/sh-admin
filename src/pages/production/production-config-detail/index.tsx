a17dcc11e0e1759b9dcaf4d8e46a9ec6b9817778
fangmingdong <fangmingdong0224@gmail.com>
2022-01-03 13:45:36 +0800

    fix: Auto commit by fe-cli.

---
+++ src/pages/production/production-config-detail/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+/**
+ * 商品配置详情页
+ */
+const ProductionConfigDetail: React.FC = () => {
+  return <div>商品配置详情页</div>
+}
+
+export default ProductionConfigDetail
---
+++ src/pages/production/production-detail/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+/**
+ * 商品详情页
+ */
+const ProductionDetail: React.FC = () => {
+  return <div>商品详情页</div>
+}
+
+export default ProductionDetail
