ffd5100cd457921efced008421e3f14e7ca12542
fangmingdong <fangmingdong0224@gmail.com>
2022-01-04 21:10:20 +0800

    fix: Auto commit by fe-cli.

--- src/pages/production/production-detail/components/BaseInfo/index.tsx
+++ src/pages/production/production-detail/components/BaseInfo/index.tsx
@@ -1,4 +1,5 @@
 import { useStore } from '@/store/context'
+import { Col, Row } from 'antd'
 import { observer } from 'mobx-react-lite'
 import React from 'react'
 import './index.less'
@@ -12,19 +13,19 @@ const BaseInfo: React.FC = () => {
   return (
     <div className="BaseInfo__root">
       <h4>1. 基本信息</h4>
-      <div className="my-info">
-        <div className="left">
+      <Row className="my-info">
+        <Col className="left" span={18}>
           <div>商品类型标签: {productionDetailStore.data?.goodsTypeTag}</div>
           <div>商品主标题: {productionDetailStore.data?.goodsName}</div>
           <div>商品副标题: {productionDetailStore.data?.goodsNickName}</div>
           <div>退改政策: {productionDetailStore.data?.refundAndChangePolicy}</div>
-        </div>
-        <div className="right">
+        </Col>
+        <Col className="right" span={6}>
           <div>创建渠道: {productionDetailStore.data?.createChannelName}</div>
           <div>创建人: {productionDetailStore.data?.createUserName}</div>
           <div>创建时间: {productionDetailStore.data?.createTime}</div>
-        </div>
-      </div>
+        </Col>
+      </Row>
     </div>
   )
 }
