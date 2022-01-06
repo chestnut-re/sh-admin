39dc4c2b6e375277bbd02d65cd8d9cf092040066
fangmingdong <fangmingdong0224@gmail.com>
2022-01-06 10:52:33 +0800

    fix: Auto commit by fe-cli.

---
+++ src/pages/production/production-config-detail/template.ts
@@ -0,0 +1,35 @@
+/**
+ * 模版数据
+ */
+
+export type TemplateType = 'face' | 'center' | 'end'
+
+/**
+ * 封面模版
+ */
+export const faceTemplate = [
+  {
+    key: 'face0',
+    templateImgUrl: '',
+  },
+]
+
+/**
+ * 中间
+ */
+export const centerTemplate = [
+  {
+    key: 'center0',
+    templateImgUrl: '',
+  },
+]
+
+/**
+ * 封底模版
+ */
+export const endTemplate = [
+  {
+    key: 'end0',
+    templateImgUrl: '',
+  },
+]
