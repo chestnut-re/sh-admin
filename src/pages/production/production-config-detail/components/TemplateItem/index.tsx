93631de116fecf613e2053e9abcdfbe28850d118
张瑞 <zhangrui@openseas.com.cn>
2022-01-06 20:05:07 +0800

    配置详情

--- src/pages/production/production-config-detail/components/TemplateItem/index.tsx
+++ src/pages/production/production-config-detail/components/TemplateItem/index.tsx
@@ -1,7 +1,10 @@
 import { useStore } from '@/store/context'
-import { Button } from 'antd'
+import { Button, Modal } from 'antd'
 import { observer } from 'mobx-react-lite'
-import React from 'react'
+import React, { useState } from 'react'
+import update from '@/assets/img/update.png'
+import del from '@/assets/img/del.png'
+import wring from '@/assets/img/wring.png'
 import './index.less'
 
 interface Props {
@@ -14,23 +17,63 @@ interface Props {
  * 模版
  */
 const TemplateItem: React.FC<Props> = ({ data, onDel, onEdit }) => {
+  const [showBox, setShowBox] = useState(false)
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const mouseOver = () => {
+    console.log('划入悬停')
+    setShowBox(true)
+  }
+  const mouseOut = () => {
+    console.log('划出')
+    setShowBox(false)
+  }
+  const handleOk = () => {
+    setIsModalVisible(false)
+    onDel?.(data.key)
+  }
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+  const delItem = () => {
+    setIsModalVisible(true)
+  }
   return (
-    <div className="TemplateItem__root">
+    <div
+      className="TemplateItem__root"
+      // onMouseOver={mouseOver}
+      // onMouseOut={mouseOut}
+    >
       <img className="bg" src={data.backgroundImage} />
-      <Button
-        onClick={() => {
-          onEdit(data.key)
-        }}
-      >
-        编辑
-      </Button>
-      <Button
-        onClick={() => {
-          onDel?.(data.key)
-        }}
-      >
-        删除
-      </Button>
+      {!showBox && (
+        <div className="btn-div">
+          <div className="btn-num">
+            <Button
+              className="btn"
+              onClick={() => {
+                onEdit(data.key)
+              }}
+            >
+              <img className="img" src={update} />
+            </Button>
+            <Button
+              className="btn"
+              onClick={() => {
+                delItem()
+              }}
+            >
+              <img className="img" src={del} />
+            </Button>
+          </div>
+        </div>
+      )}
+      <div className="bg-name">{data.pageTemplate}</div>
+      <Modal visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <div className="wring-modal">
+          {/* <img className="img-wring" src={wring} /> */}
+          <p className="wring-text">删除内容页？</p>
+          <p className="wring-title">将删除该内容页及其已填写信息内容</p>
+        </div>
+      </Modal>
     </div>
   )
 }
