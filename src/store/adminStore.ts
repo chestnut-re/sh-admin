73f15f29e04f8149bbe2a99182899fc239a519b8
邓晓坤 <dengxiaokun@openseas.com.cn>
2022-01-17 14:47:47 +0800

    fix:角色创建默认关闭

--- src/store/adminStore.ts
+++ src/store/adminStore.ts
@@ -1,6 +1,6 @@
 /*
  * @Description:
- * @LastEditTime: 2022-01-15 15:27:29
+ * @LastEditTime: 2022-01-17 14:47:39
  */
 import { USER_DETAIL } from '@/constants/CookiesC'
 import { getMenus, getDevMenus } from '@/service/menu'
@@ -34,13 +34,13 @@ class AdminData {
 
   async init() {
     // if (env != 'development') {
-      const res = await getDevMenus()
-      this.setMenu(res.data.menus)
+      // const res = await getDevMenus()
+      // this.setMenu(res.data.menus)
     // } else {
-      // const user = JSON.parse(getCookie(USER_DETAIL) ?? '')
-      // const res = await getMenus(user?.userId)
-      // this.setMenu(newMenu(res.data))
-      // this.setBtn(newBtnMenu(res.data))
+      const user = JSON.parse(getCookie(USER_DETAIL) ?? '')
+      const res = await getMenus(user?.userId)
+      this.setMenu(newMenu(res.data))
+      this.setBtn(newBtnMenu(res.data))
     // }
     if (!isUserLogin()) {
       // 未登录，去登录页面
