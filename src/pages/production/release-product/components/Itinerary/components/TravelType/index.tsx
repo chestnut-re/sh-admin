79f52461760a0a0012683f72a8661a7c197044e5
张瑞 <zhangrui@openseas.com.cn>
2022-01-18 11:58:42 +0800

    草稿箱商品空白页

--- src/pages/production/release-product/components/Itinerary/components/TravelType/index.tsx
+++ src/pages/production/release-product/components/Itinerary/components/TravelType/index.tsx
@@ -14,22 +14,22 @@ const TravelType: React.FC<Props> = ({ data }) => {
 
   if (data.travelType === 3) {
     // 酒店
-    title = data.travelGoods[travelTypeKey[data.travelType]].hotelName
+    title = data.travelGoods[travelTypeKey[data.travelType]]?.hotelName
   } else if (data.travelType === 5) {
     // 景点
-    title = data.travelGoods[travelTypeKey[data.travelType]].scenicSpotName
+    title = data.travelGoods[travelTypeKey[data.travelType]]?.scenicSpotName
   } else if (data.travelType === 4) {
     // 饭店
-    title = data.travelGoods[travelTypeKey[data.travelType]].restaurantName
+    title = data.travelGoods[travelTypeKey[data.travelType]]?.restaurantName
   } else if (data.travelType === 1) {
     // 机票
-    title = data.travelGoods[travelTypeKey[data.travelType]].airline
+    title = data.travelGoods[travelTypeKey[data.travelType]]?.airline
   } else if (data.travelType === 2) {
     // 大巴
-    title = '大巴'
+    title = data.travelGoods[travelTypeKey[data.travelType]]?.departureStation
   } else if (data.travelType === 6) {
     // 火车
-    title = '火车'
+    title = data.travelGoods[travelTypeKey[data.travelType]]?.departureStation
   }
 
   return (
